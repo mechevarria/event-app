42501326841336ab2f1ff3717ff5f6113004204e
Mike Echevarria <mike.echevarria@gmail.com>
2016-10-07 15:01:55 -0400

    remove redundant defaults

--- client/js/event/event-service.js
+++ client/js/event/event-service.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('event-app')
-  .factory('Event', ['$resource', function ($resource) {
-    return $resource('event-app/events/:id', {}, {
-      'query': { method: 'GET', isArray: true},
-      'get': { method: 'GET'},
-      'update': { method: 'PUT'}
-    });
-  }]);
+    .factory('Event', ['$resource', function($resource) {
+        return $resource('event-app/events/:id', {}, {
+            'update': {
+                method: 'PUT'
+            }
+        });
+    }]);
